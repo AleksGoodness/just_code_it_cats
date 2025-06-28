a8d06ff8914dde74649bf8bb7fc7077cc8717793
Aleks Gomeniuk <agsfinks@gmail.com>
2025-06-28 16:38:26 +0100

    feat: local storage theme toggle

--- src/components/theme-changer/ThemeChanger.tsx
+++ src/components/theme-changer/ThemeChanger.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react';
 import { Button } from '../ui/button';
+const LS_NAME = 'theme';
+type Theme = 'dark' | null;
 
 const ThemeChanger = () => {
+  const toggleTheme = () => {
+    const savedTheme = localStorage.getItem(LS_NAME) as Theme;
+    document.documentElement.classList.toggle(savedTheme || 'dark');
+    if (savedTheme) localStorage.removeItem(LS_NAME);
+    else localStorage.setItem(LS_NAME, 'dark');
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem(LS_NAME) as Theme;
+    if (savedTheme) document.documentElement.classList.add(savedTheme);
+  }, []);
+
   return (
-    <Button
-      size="lg"
-      className="font-rage text-2xl"
-      variant="secondary"
-      onClick={() => {
-        document.documentElement.classList.toggle('dark');
-      }}
-    >
+    <Button size="lg" className="font-rage text-2xl" variant="secondary" onClick={toggleTheme}>
       ThemeChanger
     </Button>
   );
